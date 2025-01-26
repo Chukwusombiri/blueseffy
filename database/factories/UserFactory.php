545cd9a050d13ac08261b0a36673b86c2f412691
Chisom Okwuosa <okwuosachisom8@gmail.com>
2025-01-26 05:30:41 +0100

    hjksls

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,6 +30,9 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'is_admin' => 1,
+            'last_sign_in_at' => now(),
+            'last_sign_out_at'=>now(),
         ];
     }
 
